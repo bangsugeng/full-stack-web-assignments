4e0506d011f16202fd3fa710fb9fb2a122f96ae0
bangsugeng <rohmadhanisss@gmail.com>
2021-11-02 09:15:46 +0700

    soal-01

---
+++ 15-React-Router/soal-01/src/pages/Movie.js
@@ -0,0 +1,45 @@
+import { Redirect, useParams } from "react-router-dom";
+import movies from "../dummy-data";
+
+
+const Movie = () => {
+  // Variable berikut akan menampung data movie yang akan kita tampilkan
+
+
+
+  let { id } = useParams();
+  let movie = movies.find((x) => x.mal_id === Number(id));
+  if(!movie) {
+    return <Redirect to = "/not-found"></Redirect>
+  }
+  return (
+    <div className="row my-5">
+      <div className="card mb-3 p-0">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={movie.image_url}
+              className="img-fluid rounded-start"
+              alt="skilvul"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{movie.title}</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-muted">{movie.score}</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
