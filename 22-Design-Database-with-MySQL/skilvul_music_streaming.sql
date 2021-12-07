351d6d656e21ae0eebc9ee5292fa282c0c5b749c
bangsugeng <rohmadhanisss@gmail.com>
2021-12-07 21:58:29 +0700

    22 done

--- 22-Design-Database-with-MySQL/skilvul_music_streaming.sql
+++ 22-Design-Database-with-MySQL/skilvul_music_streaming.sql
@@ -4,28 +4,28 @@ USE skilvul_music_streaming;
 -- create user
 CREATE TABLE user(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) NOT NULL,
-    email varchar(100) NOT NULL UNIQUE ,
-    password varchar(255) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE ,
+    password VARCHAR(255) NOT NULL
 );
 
 -- create singer
 CREATE TABLE singer(
     singer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100) NOT NULL
+    name VARCHAR(100) NOT NULL
 );
 
 -- create album
 CREATE TABLE album(
     album_id INT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     singer_id INT NOT NULL,
     CONSTRAINT FK_album_singer_id FOREIGN KEY(singer_id) REFERENCES singer(singer_id)
 );
 
 CREATE TABLE track(
     track_id INT AUTO_INCREMENT PRIMARY KEY,
-    title varchar(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
     singer_id INT NOT NULL,
     album_id INT NOT NULL,
     CONSTRAINT FK_track_singer_id FOREIGN KEY(singer_id) REFERENCES singer(singer_id),
